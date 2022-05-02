beca7be44509baf742e533b67feadedc33060066
Alexander Osterwald <alexander.osterwald@kisters.de>
2022-05-02 12:34:31 +0200

    install sudo

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ useradd -p $(openssl passwd -crypt badguy) badguy
 echo "install some apps"
 apt install ssh nano sudo -y
 echo "add user to sudoers"
-usermod -aG sudo username
+usermod -aG sudo badguy
 echo "replace catalina.sh"
 mv ./spring4shell-scripts-main/catalina.sh /usr/local/tomcat/bin/catalina.sh -f
 echo "force container restart"
